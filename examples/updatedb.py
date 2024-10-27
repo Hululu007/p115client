588161c3347f60c138624e37a9d6311bed065e9d
chenyanggao <2339083510@qq.com>
2024-10-27 17:12:59 +0800

    update.1730020379

--- examples/updatedb.py
+++ examples/updatedb.py
@@ -605,6 +605,7 @@ ON CONFLICT(id) DO UPDATE SET
     path = "/"
     for item in items:
         path = item["path"] = path + escape(item["name"])
+        path += "/"
     if commit:
         return execute_commit(con, sql, items, executemany=True)
     else:
